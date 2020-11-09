eb76d884c7fab34d52afdd67dafbe93e997016ca
Marissa Aguilera <marissaguilera@yahoo.com>
2020-11-08 20:26:30 -0800

    A vehcile class with mileage/ max speed

--- vehicle.py
+++ vehicle.py
@@ -1,3 +1,14 @@
 
 # Exercise Question 1: Create a Vehicle class with max_speed and 
 # mileage instance attributes
+
+class Vehicle:
+    """A vehicle."""
+
+    def __init__(self, max_speed, mileage):
+        """Initializing."""
+
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+        
